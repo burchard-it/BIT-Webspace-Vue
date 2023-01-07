ff909ee7a47ead6daa54773340713f42fdc5f6ea
Martin Burchard <dev@burchard-it.de>
2023-01-07 11:05:44 +0100

    add Vuetify initialisation

--- src/vue/main.ts
+++ src/vue/main.ts
@@ -1,11 +1,10 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import router from '@/router';
-
-import '@/scss/main.scss';
+import vuetify from '@/plugins/vuetify';
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router).use(vuetify);
 
 app.mount('#app');
