a5811762ecbe1229e400351d7b54709db2608f68
Martin Burchard <dev@burchard-it.de>
2023-02-04 17:03:44 +0100

    remove routes that are not used at the moment

--- src/vue/router/index.ts
+++ src/vue/router/index.ts
@@ -1,25 +1,28 @@
+import type {RouteRecordRaw} from 'vue-router';
 import {createRouter, createWebHistory} from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+// import HomeView from '@/views/HomeView.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  // {
+  //   name: 'home',
+  //   path: '/',
+  //   component: HomeView,
+  // },
+  // {
+  //   name: 'about',
+  //   path: '/about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (About.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import('@/views/AboutView.vue'),
+  // },
+];
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   // workaround for https://github.com/lhapaipai/vite-bundle/commit/0bbbc4649ec8ed1bfc39b973b2aed043f4dc8c30#commitcomment-95341866
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue'),
-    },
-  ],
+  routes,
 });
 
 export default router;
