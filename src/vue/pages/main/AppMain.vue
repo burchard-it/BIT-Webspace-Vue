4f0565a132d3e620461c7821acb07447e8400819
Martin Burchard <dev@burchard-it.de>
2023-02-07 16:27:50 +0100

    move existing Vue page into main

---
+++ src/vue/pages/main/AppMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-xl py-3">
+    <h1>BIT Webspace Vue Frontend</h1>
+    <p>Bootstrap is installed</p>
+    <p>Current Count {{ counter }}
+      <button class="btn btn-outline-primary" @click="doCount">Count</button>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+const counter = ref(0);
+
+function doCount() {
+  counter.value++;
+}
+</script>
