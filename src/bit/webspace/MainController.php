afd2cbb19e5c176e1e256255c48a19bad145b564
Martin Burchard <dev@burchard-it.de>
2023-02-07 13:00:43 +0100

    remove png and ico rule and move MainController to new location

--- src/bit/webspace/MainController.php
+++ src/bit/webspace/MainController.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace App\Controller;
+namespace App\bit\webspace;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends DefaultController {
 
-  #[Route('/{path<.+>}', name: 'homepage',
-    requirements: ['path' => '^(?!api\/|.+\.png\/?$|.+\.ico\/?$|.+\.xml\/?$|.+\.php).+'])]
+  #[Route('/{path<.+>}', name: 'homepage', requirements: ['path' => '^(?!api\/|.+\.xml\/?$|.+\.php).+'])]
   public function indexAction($path = ''): Response {
     $this->logger->debug('Here I am', [
       'path' => $path,
